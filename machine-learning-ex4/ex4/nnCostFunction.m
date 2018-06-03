232470c30ee12fa18310c35ee57f04e0380aaf08
Guowei He <guoweihe@Guoweis-MacBook-Pro.local>
2018-06-03 00:00:38 -0700

    finish

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -76,10 +76,22 @@ error = (lambda/(2*m)) * (sum(sum(regTheta1.^2)) + sum(sum(regTheta2.^2)));
 
 J = J + error;
 
+del1 = zeros(size(Theta1));
+del2 = zeros(size(Theta2));
 
+for t = 1 : m,
+	a1t = a1(t, :);
+	a2t = a2(t, :);
+	a3t = a3(t, :);
+	yt = y(t, :);
+	d3 = a3t - yt;
+	d2 = Theta2' * d3' .* sigmoidGradient([1;Theta1 * a1t']);
+	del1 = del1 + d2(2:end) * a1t;
+	del2 = del2 + d3' * a2t;
+end;
 
-
-
+Theta1_grad = 1/m * del1 + (lambda/m)*[zeros(size(Theta1, 1), 1) regTheta1];
+Theta2_grad = 1/m * del2 + (lambda/m)*[zeros(size(Theta2, 1), 1) regTheta2];
 
 
 
